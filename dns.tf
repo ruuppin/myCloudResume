98673b787173276056fb84c54eaf20c66a2021b4
ruuppin <robin.ingves@hotmail.com>
2025-12-06 23:43:47 +0200

    ttt

--- dns.tf
+++ dns.tf
@@ -12,3 +12,10 @@ resource "azurerm_dns_a_record" "www" {
   records             = ["10.10.1.1"]
   
 }
+resource "azurerm_dns_cname_record" "blog" {
+  name                = "blog"
+  zone_name           = azurerm_dns_zone.ruuppin.name
+  resource_group_name = azurerm_resource_group.resume_resources.name
+  ttl                 = 300
+  record              = "blogs.ruuppin.com"
+}
